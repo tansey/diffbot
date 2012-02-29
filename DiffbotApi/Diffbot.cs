cb5caa924a292278fec62c8bd287b677a82097c3
Thomas Thornton <thomas.thornton.jr@gmail.com>
2012-02-29 15:31:09 -0700

    added diffbot constructor which takes a WebProxy

--- DiffbotApi/Diffbot.cs
+++ DiffbotApi/Diffbot.cs
@@ -14,23 +14,28 @@ namespace DiffbotApi
         private const string ARTICLE_API_SUFFIX = "article";
         private const string FRONTPAGE_API_SUFFIX = "frontpage";
         public string DeveloperToken { get; set; }
+        private WebProxy _proxy;
 
         public Diffbot(string token)
         {
             DeveloperToken = token;
         }
 
+        public Diffbot(string token, WebProxy proxy) : this(token)
+        {
+            _proxy = proxy;
+        }
+
         public Frontpage Frontpage(string url)
         {
             using (WebClient wc = new WebClient())
             {
-                wc.Proxy = null;
+                wc.Proxy = _proxy;
 				wc.Encoding = Encoding.UTF8;
                 string xmlString = wc.DownloadString(getQueryUrl(FRONTPAGE_API_SUFFIX, url));
                 XmlSerializer ser = new XmlSerializer(typeof(Frontpage));
                 return (Frontpage)ser.Deserialize(new StringReader(xmlString));
             }
-
         }
 
         public Article Article(string url, bool comments = false, bool keepAds = false, bool stats = false, bool summary = false, bool tags = false)
